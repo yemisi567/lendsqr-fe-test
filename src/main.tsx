4b37e1dad54acad86be106d5a54fd543a07e3168
yemisi567 <alegbeyemi@gmail.com>
2025-03-11 14:46:26 +0100

    Fix build failure

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import App from "./App";
 import "./styles/globals.scss";
 import { AuthProvider } from "./context/Auth/AuthProvider";
 import { UsersProvider } from "./context/Users/UsersProvider";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster position="top-right" richColors />
       <AuthProvider>
         <UsersProvider>
           <App />
