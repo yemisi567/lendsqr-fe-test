6c63fec3651424a2c94a909d08c6aad5023c2f28
yemisi567 <alegbeyemi@gmail.com>
2025-03-11 12:10:12 +0100

    Fix some bugs and UI changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./styles/globals.scss";
-import { Toaster } from "sonner";
 import { AuthProvider } from "./context/Auth/AuthProvider";
 import { UsersProvider } from "./context/Users/UsersProvider";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Toaster position="top-right" richColors />
       <AuthProvider>
         <UsersProvider>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
+          <App />
         </UsersProvider>
       </AuthProvider>
     </BrowserRouter>
